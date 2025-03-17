ee83ed6a4f5bdac501e7f7555d8e41d290cfc188
Winkas09 <edvinas17935@gmail.com>
2025-03-17 18:24:19 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-center py-10">
+      <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
+      <p className="mt-4 text-lg">This is a simple Next.js application.</p>
+    </div>
+  );
+}
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: c:\Users\ITwork\Desktop\front-end\cdl\my-nextjs-app\postcss.config.js
+module.exports = {
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
+    },
+  }
